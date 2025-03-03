40860328c0e9e0bb5fac86f47603bd5a96186749
Dillon Marinez <dimarinez@yahoo.com>
2025-03-03 15:25:56 -0800

    adds next auth secreet

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -7,6 +7,7 @@ import connectDB from './db';
 import User from '../models/User';
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
